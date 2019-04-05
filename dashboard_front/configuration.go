91c8730ff0905beee84db38eb4d5d8f357f92531
813743 <carlosisrael.zendejas@fedex.com>
2019-04-05 17:01:01 -0600

    18 commit- adjust port 8060

--- dashboard_front/configuration.go
+++ dashboard_front/configuration.go
@@ -41,7 +41,7 @@ var Config_connString string
 
 func init() {
 	flag.StringVar(&RunMode, "mode", "api", "Service mode run (Options: api, batch)")
-	flag.StringVar(&HTTPListen, "http", ":8070", "Path where HTTP Server will listening")
+	flag.StringVar(&HTTPListen, "http", ":8060", "Path where HTTP Server will listening")
 	flag.StringVar(&configFile, "config", "./conf/config.json", "Path of configuration file")
 //	flag.StringVar(&configFile, "config", "config.json", "Path of configuration file")
 
