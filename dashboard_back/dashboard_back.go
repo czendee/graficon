0c7130914fff2ef21ae69c0db48be78892ed0c33
813743 <carlosisrael.zendejas@fedex.com>
2019-05-15 07:30:57 -0500

    sue ticker instead og gocron

--- dashboard_back/dashboard_back.go
+++ dashboard_back/dashboard_back.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-//	"banwire/dash/dashboard_back/net"
-//	conf "banwire/dash/dashboard_back/conf"
-//	"flag"
-//	"log"
-//	"sync"
-    "github.com/jasonlvhit/gocron" // Use here: go get github.com/jasonlvhit/gocron
         "time"
         "fmt"
 )
 
-//var loaded sync.WaitGroup
 
 const (
 	DefaultRunMode = ""
@@ -24,84 +17,8 @@ func init() {
 	
 }
 
-/*
-func main() {
-//	flag.Parse()
-//	LoadConfiguration()
-
-//	loaded.Done()
-
-	log.Print("Jobs is ready for run...")
-
-	// Do jobs with params
-//	gocron.Every(3).Second().Do(taskWithParams, 1, "hello")
-
-	// Do jobs without params
-//	gocron.Every(5).Second().Do(task)
-	gocron.Every(1).Seconds().Do(task)
-    log.Print("Jobs is running")
-      time.Sleep(10 * time.Second)
-      log.Print("Jobs is running after sleep")
 
 
-	switch RunMode {
-	case BatchRunMode:
-		BatchTest()
-		break
-	case DefaultRunMode, ApiRunMode:
-		startServer()
-		break
-	default:
-		log.Print("Run mode is unknown")
-		break
-	}
-
-}
-
-*/
-
-/*
-
-func startServer() {
-	log.Printf("HTTP Listening: %s", HTTPListen)
-	net.HTTPListener(HTTPListen)
-}
-*/
-
-/*
-
-func task() {
-		log.Print("I am runnning task.")
-}
-
-func taskWithParams(a int, b string) {
-		log.Print(a, b)
-}
-
-*/
-func jobs(quit <-chan bool) {
-    for {
-        //cron jobs
-        g := gocron.NewScheduler()
-
-        g.Every(uint64(Config_task_1periocity)).Seconds().Do(dashBack01Grafica01)
-        g.Every(uint64(Config_task_2periocity)).Second().Do(stuff)
-
-//        g := gocron.NewScheduler()
-//        g.Every(1).Second().Do(stuff)
-
-
-        select {
-        case <-quit:
-            // here we receive from quit and exit
-            // if `g` has started, we may want to `g.Clear()`
-            // or the scheduled jobs will continue, we will just not be waiting for them to finish.
-            return
-        case <-g.Start():
-            // here we know all the jobs are done, and go round the loop again
-        }
-    }
-}
 func stuff() {
     fmt.Println("doing job")
 }
@@ -125,21 +42,30 @@ func dashBack01Grafica01() {
 
 
 
-func main() {
-    fmt.Println("main   01")
 
-    //	flag.Parse()
+func main() {
+    fmt.Println("main  ticker  01")
+
+    
 	LoadConfiguration()
 
-//	loaded.Done()
-fmt.Println("main   02")
-    q := make(chan bool)
-    go jobs(q)
+
+fmt.Println("main  ticker 02")
+    
+    ticker:= time.NewTicker(time.Duration(Config_task_1periocity)*time.Second)
+fmt.Println("main ticker  03")    
+    go func(){
+          for t:=range ticker.C{
+              fmt.Println("main ticker  05:",t)
+               dashBack01Grafica01()
+          }
+    }()
+
     
     time.Sleep(time.Duration(Config_task_timeframe) * time.Second)
-
+    fmt.Println("main ticker  06")
  //to quit the goroutine
-    q <- true    
-    close(q)
-    fmt.Println("main   03")
+    ticker.Stop()    
+    
+    fmt.Println("main ticker  07")
 }
