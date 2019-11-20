cf345d6ac060d50ed3f8f41d5a4b2385108c954b
Don Carlos Zendejas <czende@hotmail.com>
2019-11-19 19:35:06 -0600

    Update real-time_dash27.js
    add rotation from dash07 to dash03

--- dashboard_front/bin_dashboard_front/css/real-time_dash27.js
+++ dashboard_front/bin_dashboard_front/css/real-time_dash27.js
@@ -1038,7 +1038,7 @@ function updateHOYChartWith30diasMenos2(item, index) {
 
     function go_next2() {
 //        alert("next");
-        window.location.href = "/v2/dash02";  
+        window.location.href = "/v2/dash03";  
 
     }
 
@@ -1052,7 +1052,7 @@ function updateHOYChartWith30diasMenos2(item, index) {
                 updateChartsAtRandomIntervals();
         setInterval(checkRotationValues, valueRotacionOriginal);//every 20 seconds
 
- //       setInterval(go_next2, valuePasarNextOriginal);//every 150 seconds
+        setInterval(go_next2, valuePasarNextOriginal);//every 150 seconds
 
 		sidebarToggleOnClick();
 	})();
